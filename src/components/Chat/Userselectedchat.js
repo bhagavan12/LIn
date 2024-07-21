2c0d5cdd6171444af8fc012b8e55c2d414e9eca1
bhagavan12 <jsbteja123@gmail.com>
2024-07-21 23:56:48 +0530

    userselectedchat updated

--- src/components/Chat/Userselectedchat.js
+++ src/components/Chat/Userselectedchat.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect ,useRef} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { io } from 'socket.io-client';
 import axios from 'axios';
 import { ref, getDownloadURL, listAll } from "firebase/storage";
@@ -8,16 +8,16 @@ import './Chat.css';
 
 const socket = io('https://chatsocketv2-latest.onrender.com');
 
-const Chat = ({ loggedInUserId ,friendUserId}) => {
+const Chat = ({ loggedInUserId, friendUserId }) => {
   const [messages, setMessages] = useState([]);
   const [input, setInput] = useState('');
   const ele = useRef(null);
-//   const { friendUserId } = useParams();
+  //   const { friendUserId } = useParams();
   const [profileImageUrl, setProfileImageUrl] = useState(null);
   const [profileImageUrlf, setProfileImageUrlf] = useState(null);
   useEffect(() => {
-     // Listen for new messages
-     socket.on('message', (message) => {
+    // Listen for new messages
+    socket.on('message', (message) => {
       if (
         (message.user1 === loggedInUserId && message.user2 === friendUserId) ||
         (message.user1 === friendUserId && message.user2 === loggedInUserId)
@@ -49,7 +49,7 @@ const Chat = ({ loggedInUserId ,friendUserId}) => {
 
     fetchChat();
 
-   
+
   }, [loggedInUserId, friendUserId]);
 
   const getProfileImageUrl = async (userId) => {
@@ -71,12 +71,12 @@ const Chat = ({ loggedInUserId ,friendUserId}) => {
   };
   function formatTimestamp(timestamp) {
     const date = new Date(timestamp);
-  
+
     // Extract date components
     const day = date.getDate();
     const month = date.getMonth() + 1; // Months are 0-based
     const year = date.getFullYear();
-  
+
     // Extract time components
     let hours = date.getHours();
     const minutes = date.getMinutes();
@@ -84,11 +84,11 @@ const Chat = ({ loggedInUserId ,friendUserId}) => {
     const ampm = hours >= 12 ? 'PM' : 'AM';
     hours = hours % 12;
     hours = hours ? hours : 12; // The hour '0' should be '12'
-  
+
     // Format the components
     const formattedDate = `${month}/${day}/${year}`;
     const formattedTime = `${hours}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds} ${ampm}`;
-  
+
     return `${formattedDate} ${formattedTime}`;
   }
   const sendMessage = () => {
@@ -97,14 +97,14 @@ const Chat = ({ loggedInUserId ,friendUserId}) => {
     setInput('');
   };
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [objToDel,setobjToDel]=useState(null);
-  const [timeofObjd,settimeofObjd]=useState(null);
-  const toggleModal = (obj,time) => {
+  const [objToDel, setobjToDel] = useState(null);
+  const [timeofObjd, settimeofObjd] = useState(null);
+  const toggleModal = (obj, time) => {
     setobjToDel(obj);
     settimeofObjd(time);
     setIsModalOpen(!isModalOpen);
   };
-  
+
   const funcDel = async () => {
     try {
       await axios.delete(`https://chatsocketv2-latest.onrender.com/chat/${friendUserId}/message/${objToDel}?loggedInUserId=${loggedInUserId}`);
@@ -122,13 +122,16 @@ const Chat = ({ loggedInUserId ,friendUserId}) => {
             {msg.sender !== loggedInUserId && profileImageUrlf && (
               <img src={profileImageUrlf} alt="Profile" className="profile-image" />
             )}
-            <div className={`msgbox`}>
-             {(msg.sender === loggedInUserId) ? <i className='ph--dots-three-circle-vertical-light' style={{ margin: "auto 0px" }} onClick={() => toggleModal(msg._id,formatTimestamp(msg.timestamp))}></i> : <i className=''></i>}
-              <div ref={ele} className={`message-content ${msg.sender === loggedInUserId ? 'message-right' : 'message-left'}`}>
-                {msg.message}
+            <div style={{ display: "flex" }}>
+              {(msg.sender === loggedInUserId) ? <i className='ph--dots-three-circle-vertical-light' style={{ margin: "auto 0px" }} onClick={() => toggleModal(msg._id, formatTimestamp(msg.timestamp))}></i> : <i className=''></i>}
+              <div className={`msgbox`}>
 
+                <div ref={ele} className={`message-content ${msg.sender === loggedInUserId ? 'message-right' : 'message-left'}`}>
+                  {msg.message}
+
+                </div>
+                <div style={{ fontSize: "small", textAlign: "end" }}>{formatTimestamp(msg.timestamp)}</div>
               </div>
-              <div style={{ fontSize: "small" ,textAlign:"end"}}>{formatTimestamp(msg.timestamp)}</div>
             </div>
           </div>
         ))}
@@ -137,11 +140,11 @@ const Chat = ({ loggedInUserId ,friendUserId}) => {
         <input className='typeinput' value={input} onChange={(e) => setInput(e.target.value)} />
         <button className='cbutton' onClick={sendMessage}>Send</button>
       </div>
-      {isModalOpen && <Modal onClose={toggleModal} onDelete={funcDel} time={timeofObjd}/>}
+      {isModalOpen && <Modal onClose={toggleModal} onDelete={funcDel} time={timeofObjd} />}
     </div>
   );
 };
-const Modal = ({ onClose, onDelete,time }) => {
+const Modal = ({ onClose, onDelete, time }) => {
   const handleBackgroundClick = (e) => {
     if (e.target.classList.contains('modal')) {
       onClose();
@@ -151,9 +154,9 @@ const Modal = ({ onClose, onDelete,time }) => {
     <div className="modal" onClick={handleBackgroundClick}>
       <div className="modal-content">
         {/* <span className="close" onClick={onClose}>&times;</span> */}
-        <span style={{padding:"15px"}}>{time}</span>
-        <hr/>
-        
+        <span style={{ padding: "15px" }}>{time}</span>
+        <hr />
+
         <span className="deleteb" onClick={onDelete}>Unsend</span>
       </div>
     </div>
