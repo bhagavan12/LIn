cc2cf207d7a281418084853329a37324d3f31aea
bhagavan12 <jsbteja123@gmail.com>
2024-07-22 16:38:02 +0530

    qwerty

--- src/firebase.js
+++ src/firebase.js
@@ -11,17 +11,10 @@ const firebaseConfig = {
   messagingSenderId: String(process.env.REACT_APP_MSG),
   appId: String(process.env.REACT_APP_APPID)
 };
-// console.log("apiKey",firebaseConfig.apiKey)
-// alert(firebaseConfig.apiKey);
+
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app); // Firebase Storage
 const db = getFirestore(app); // Firestore
 const auth = getAuth(app);
 export { storage, db ,auth};
 
-// apiKey: "AIzaSyA15NS9loNBl_QJ0nVsB8ohjdgsgD8YYGQ",
-// authDomain: "tej-19-4-24.firebaseapp.com",
-// projectId: "tej-19-4-24",
-// storageBucket: "tej-19-4-24.appspot.com",
-// messagingSenderId: "804160138505",
-// appId: "1:804160138505:web:9456e5f27337c5db5837d8"
