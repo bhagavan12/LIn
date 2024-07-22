8d416719f1c3134019f6c45778b4818f457eef13
bhagavan12 <jsbteja123@gmail.com>
2024-07-22 17:04:02 +0530

    wed

--- src/firebase.js
+++ src/firebase.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const storage = getStorage(app); // Firebase Storage
+const storage = getStorage(app); 
 const db = getFirestore(app); // Firestore
 const auth = getAuth(app);
 export { storage, db ,auth};
